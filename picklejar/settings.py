146bc8fdb7ddfb3b7e0401ff13ea483a969f3150
Fabio Romeiro <fabio.romeiro@buser.com.br>
2021-06-05 08:50:25 -0300

    Flexibiliza porta do redis

--- picklejar/settings.py
+++ picklejar/settings.py
@@ -114,7 +114,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [(os.getenv('REDIS_HOST', 'localhost'), 6379)]
+            'hosts': [(os.getenv('REDIS_HOST', 'localhost'), os.getenv('REDIS_PORT', 6379))]
         }
     }
 }
