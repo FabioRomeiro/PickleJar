a9a5f4e66b38f5cfbecd469d3988d51147e97b11
Fabio Romeiro <fabio.romeiro@buser.com.br>
2021-06-05 08:45:27 -0300

    Flexibiliza conexão com redis

--- picklejar/settings.py
+++ picklejar/settings.py
@@ -114,7 +114,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('172.18.0.3', 6379)]
+            'hosts': [(os.getenv('REDIS_HOST', 'localhost'), 6379)]
         }
     }
 }
