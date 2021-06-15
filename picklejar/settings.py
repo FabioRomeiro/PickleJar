07ecd7d590b41440898651d53bb7e2d4ebdcdebf
Fabio Romeiro <fabio.romeiro@buser.com.br>
2021-06-14 22:17:45 -0300

    Deixa CORS mais abrangente

--- picklejar/settings.py
+++ picklejar/settings.py
@@ -208,17 +208,8 @@ LOGGING = {
 EXPLORER_CONNECTIONS = {'default': 'default'}
 EXPLORER_DEFAULT_CONNECTION = 'default'
 
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_WHITELIST = [
-    'http://picklejar.fabioromeiro.dev.s3-website-us-west-2.amazonaws.com',
-    'http://picklejar.fabioromeiro.dev',
-    'https://picklejar.fabioromeiro.dev',
-]
-CORS_ORIGIN_REGEX_WHITELIST = [
-    'http://picklejar.fabioromeiro.dev.s3-website-us-west-2.amazonaws.com',
-    'http://picklejar.fabioromeiro.dev',
-    'https://picklejar.fabioromeiro.dev',
-]
 
 if DEBUG:
     # make all loggers use the console.
